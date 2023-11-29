aa045a817ca5803afabe596d967f72467f2bfd12
Dagest <41796094+yangziyu1230@users.noreply.github.com>
2023-11-29 21:46:56 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,7 +19,7 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/page?sid=4fa2da89830df477574fd2ede4165f7f&uid=2843794&pid=19841328f10ef1667812ccff052f6f9e&tid=98895e9aeea5bbdc2cc946163d5ee164&view_time=1701177216&bind_user=0&create_user=0&tid=98895e9aeea5bbdc2cc946163d5ee164&sid=4fa2da89830df477574fd2ede4165f7f&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDitz25aqSORthmZc9vxF6-FEA&token=27b50be5a67d14a196928a68cf7da1c30c494d0c7b72e50ac8fd13ea685de2a6",
+    "https://www.maimemo.com/page?sid=1b0d1802686a47a3cd76edee4d235f48&uid=2843794&pid=ccc24be8120691e092c897bb9fd53f80&tid=8cc15430f9ba1bd9c2eb02ab86358ccd&view_time=1701265517&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDitz25aqSORthmZc9vxF6-FEA&token=f09820daf570820080324efc67bfd7e3a5f3954a87bb6c995fabc6748d87a4ef",
   ]
   /**
    * 生成随机数字
