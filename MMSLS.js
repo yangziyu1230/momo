32c331dd514732c6af36c5bb5e7516acd2a33eb0
Dagest <41796094+yangziyu1230@users.noreply.github.com>
2024-03-19 14:30:54 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,7 +19,7 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/page?uid=2843794&pid=60042dfc5107bb07fdaf44cc57d24b33&tid=d4537fd09383b35fb18c98f3be04c522",
+    "https://www.maimemo.com/share/page?uid=2843794&pid=13674b834b884a0e0e20b6c547fe72ec&tid=40eb9f0e8591182405a54b2f1bb88992",
   ]
   /**
    * 生成随机数字
